72cbd27c5a157469fa9787a27978802543deb55e
Nemanja5200 <nemanja.bogojevic@shindiristudio.com>
2025-08-21 15:16:19 +0200

    Roll Button

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@
 import {Header} from "./components/Header.jsx";
 import './App.css';
 import {DiceSection} from "./components/DiceSection.jsx";
+import {Roll} from "./components/Roll.jsx";
 function App() {
 
   return (
     <>
 
       <div className="main-container">
-        <Header/>
+          <Header/>
           <DiceSection/>
+          <Roll/>
       </div>
 
 
---
+++ src/components/Roll.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../css/Roll.css";
+export const Roll = () => {
+    return (
+        <>
+            <div className="roll-container">
+                <button> Roll</button>
+            </div>
+        </>
+    );
+};
